f17932ac65f21f8ac913cf2792197f7a744a30a6
Alexander Kharkovskiy <avkharkovskiy@gmail.com>
2017-10-31 00:19:43 +0400

    Fix controller

--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -4,34 +4,14 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public JsonResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Json("Hello Word");
         }
     }
 }
