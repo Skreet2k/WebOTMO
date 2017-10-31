679b3ec5cac714ed7b4b559c4534d909824beb56
mefest0 <shargefl@gmail.com>
2017-10-31 12:40:16 +0400

    Add chart 9a9

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './biz/chart/chart.component';
+import { LineChartComponent } from './biz/chart/lineChart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    LineChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
