5c61d339e9653299b9e27b840eb13a0f7f8e5c8d
Alex Kh <avkharkovskiy@gmail.com>
2017-12-09 21:03:39 +0400

    add message when log/pass invalid

--- WebClient/src/app/login/login.component.ts
+++ WebClient/src/app/login/login.component.ts
@@ -5,39 +5,45 @@ import { AlertService } from '../services/alert.service';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'login.component.html'
+  moduleId: module.id,
+  templateUrl: 'login.component.html'
 })
-
 export class LoginComponent implements OnInit {
-    model: any = {};
-    loading = false;
-    returnUrl: string;
+  model: any = {};
+  loading = false;
+  returnUrl: string;
 
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-        private alertService: AlertService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService
+  ) {}
 
-    ngOnInit() {
-        // reset login status
-        this.authenticationService.logout();
+  ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
 
-    login() {
-        this.loading = true;
-        this.authenticationService.login(this.model.username, this.model.password)
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.alertService.error(error._body);
-                    this.loading = false;
-                });
-    }
+  login() {
+    this.loading = true;
+    this.authenticationService
+      .login(this.model.username, this.model.password)
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.loading = false;
+          if (error.status === 401) {
+            this.alertService.error('Invalid username or password');
+            return;
+          }
+          this.alertService.error('Somethink wrong. Try again later');
+        }
+      );
+  }
 }
