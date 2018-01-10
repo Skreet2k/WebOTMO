63d564ac39840096fbf5cb2542de48fba55f413b
Alex Kh <avkharkovskiy@gmail.com>
2018-01-11 00:49:59 +0400

    fix type of DeltaX

--- WebApi/Entities/Function.cs
+++ WebApi/Entities/Function.cs
@@ -3,7 +3,7 @@ namespace WebApi.Entities {
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public float DeltaX { get; set; }
+        public double DeltaX { get; set; }
         public bool IsNeedMaxLoadFactor { get; set; }
     }
 }
