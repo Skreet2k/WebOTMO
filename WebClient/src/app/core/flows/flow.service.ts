edc9f9667aa1f09de8316a195708d48a3a434340
Alexander Kharkovskiy <avkharkovskiy@gmail.com>
2018-01-13 20:59:49 +0400

    add cache for functions and flows (only get all methods)

--- WebClient/src/app/core/flows/flow.service.ts
+++ WebClient/src/app/core/flows/flow.service.ts
@@ -2,44 +2,67 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { AppConfig } from '../../app.config';
 import { SecurityPrincipal } from '../auth/securityPrincipal';
-import * as _ from "lodash";
+import * as _ from 'lodash';
+import { StorageService } from '../../services/storage.service';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class FlowService {
     constructor(
         private readonly http: Http,
-        private readonly config: AppConfig) {
+        private readonly config: AppConfig,
+        private storage: StorageService) {
     }
 
+    flowLifeMs = 60000;
+
     public getAll(): Promise<Flow[]> {
+        let flowStorage = this.storage.get('flows') as FlowStorage;
+        if (flowStorage && new Date(flowStorage.updateDate) > this.getLifeDate()) {
+            return Observable.of(flowStorage.flows).toPromise();
+        }
         return this.http.get(this.config.apiUrl + '/flow/get', SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
-            .then((response: Response) => response.json());
+            .then((response: Response) => {
+                const result = response.json();
+                flowStorage = { updateDate: new Date(), flows: result };
+                this.storage.set('flows', flowStorage);
+                return result;
+            });
     }
 
     public getById(id: string): Promise<Flow> {
-        return this.http.get(this.config.apiUrl + `/flow/get/${id}`, SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
+        return this.http.get(this.config.apiUrl + `/flow/get/${id}`,
+            SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
             .then((response: Response) => response.json());
     }
 
     public upload(name: string, data: string): Promise<any> {
         return this.http.post(this.config.apiUrl + '/flow/upload',
-                { name: name, data: data },
-                SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
+            { name: name, data: data },
+            SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
             .then((response: Response) => response.json());
     }
 
     public update(flow: Flow): Promise<any> {
-        return this.http.put(this.config.apiUrl + '/flow/update', flow, SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
+        return this.http.put(this.config.apiUrl + '/flow/update', flow,
+            SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
             .then((response: Response) => response.json());
     }
 
     public remove(flow: Flow): Promise<any> {
         const id = flow.id;
-        return this.http.delete(this.config.apiUrl + `/flow/delete/${id}`, SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
+        return this.http.delete(this.config.apiUrl + `/flow/delete/${id}`,
+            SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise();
     }
 
     public validate(flow: Flow) {
         return true;
+    }
+
+    private getLifeDate(): Date {
+        const date = new Date(new Date().getTime() - this.flowLifeMs);
+        return date;
     }
 }
 
@@ -47,6 +70,11 @@ export class Flow {
     id: number;
     name: string;
     data: string;
-    userId?: string
+    userId?: string;
+}
+
+export class FlowStorage {
+    updateDate: Date;
+    flows: Array<Flow>;
 }
 
--- WebClient/src/app/core/flows/flowFunctions.service.ts
+++ WebClient/src/app/core/flows/flowFunctions.service.ts
@@ -1,27 +1,48 @@
-import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { AppConfig } from '../../app.config';
 import { Flow } from './flow.service';
 import { SecurityPrincipal } from '../auth/securityPrincipal';
-import * as _ from "lodash";
+import * as _ from 'lodash';
+import { StorageService } from '../../services/storage.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
 
 @Injectable()
 export class FlowFunctionsService {
     constructor(
         private readonly http: Http,
-        private readonly config: AppConfig) {
+        private readonly config: AppConfig,
+        private storage: StorageService) {
     }
 
+    functionLifeMs = 600000;
+
     public getAll(): Promise<FunctionDisplayItem[]> {
+        let functionStorage = this.storage.get('functions') as FunctionDisplayStorage;
+        if (functionStorage && new Date(functionStorage.updateDate) > this.getLifeDate()) {
+            return Observable.of(functionStorage.functions).toPromise();
+        }
         return this.http.get(this.config.apiUrl + '/function/get', SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
-            .then((response: Response) => response.json());
+            .then((response: Response) => {
+                const result = response.json();
+                functionStorage = { updateDate: new Date(), functions: result };
+                this.storage.set('functions', functionStorage);
+                return result;
+            });
     }
 
     public processFunction(args: ProcessFunctionRequestArgs): Promise<number[]> {
         return this.http.post(this.config.apiUrl + '/function/processFunction',
             args,
             SecurityPrincipal.createRequestOptionsWithUserJSONWebToken()).toPromise()
-        .then((response: Response) => response.json());
+            .then((response: Response) => response.json());
+    }
+
+    private getLifeDate(): Date {
+        const date = new Date(new Date().getTime() - this.functionLifeMs);
+        return date;
     }
 }
 
@@ -31,6 +52,11 @@ export class FunctionDisplayItem {
     public description: string;
     public deltaX: number;
     public isNeedMaxLoadFactor: boolean;
+}
+
+export class FunctionDisplayStorage {
+    updateDate: Date;
+    functions: Array<FunctionDisplayItem>;
 }
 
 export class ProcessFunctionRequestArgs {
